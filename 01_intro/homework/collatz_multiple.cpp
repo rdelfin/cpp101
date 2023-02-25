db4bf2464fc2d046ca21b4a7d5a32ae1f3575e12
arvind <arvind@wayve.ai>
2023-02-25 11:47:03 +0000

    part2 works but..

--- 01_intro/homework/collatz_multiple.cpp
+++ 01_intro/homework/collatz_multiple.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
 int main() {
-    std::cout << "1" << std::endl;
+
+    int user_input;
+    uint32_t count = 0;
+    std::cout << "Type number: "; // Type a number and press enter
+    std::cin >> user_input; // Get user input from the keyboard
+    std::cout << "Your number is: " << user_input << std::endl; // Display the input val   
+
+
+    if(user_input <=0){
+        std::cout << "Please enter a non-zero positive integer!" << std::endl;
+    }
+    else{
+        uint32_t number = user_input ;
+        while(number !=1){
+            if(number%2 == 0){
+                number = number / 2;
+                count = count + 1;
+            }
+            else{
+                number = number*3 + 1;
+                count = count + 1;
+            }
+        }
+    }
+    std::cout << "Collatz's sequence has " << count << " elements" << std::endl;
     return 0;
 }
