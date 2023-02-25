535552335e52ab51576a99487cca7c132c0481d0
arvind <arvind@wayve.ai>
2023-02-25 12:14:32 +0000

    part2 multiple, no arrays

--- 01_intro/homework/collatz_multiple.cpp
+++ 01_intro/homework/collatz_multiple.cpp
@@ -2,29 +2,35 @@
 
 int main() {
 
-    int user_input;
-    uint32_t count = 0;
-    std::cout << "Type number: "; // Type a number and press enter
-    std::cin >> user_input; // Get user input from the keyboard
-    std::cout << "Your number is: " << user_input << std::endl; // Display the input val   
+    int num_of_elements;
+    int input;
+    uint32_t count;
+    std::cout << "Enter elements number of elements you wish to enter: "; // Type a number and press enter
+    std::cin >> num_of_elements; // Get user input from the keyboard
 
-
-    if(user_input <=0){
-        std::cout << "Please enter a non-zero positive integer!" << std::endl;
+    if (num_of_elements <=0){
+        std::cout << "Please enter positive number of elements" << std::endl;
     }
-    else{
-        uint32_t number = user_input ;
-        while(number !=1){
-            if(number%2 == 0){
-                number = number / 2;
-                count = count + 1;
+    for(int i=0; i<num_of_elements; i++){
+        std::cin >> input;
+        if(input <=0){
+            std::cout << "Please enter a non-zero positive integer!" << std::endl;
+        }
+        else{
+            uint32_t number = input;
+            count = 0;
+            while(number !=1){
+                if(number%2 == 0){
+                    number = number / 2;
+                    count = count + 1;
+                }
+                else{
+                    number = number*3 + 1;
+                    count = count + 1;
+                }
             }
-            else{
-                number = number*3 + 1;
-                count = count + 1;
-            }
+            std::cout << input << "'s Collatz'z sequence has " << count << " elements" << std::endl;
         }
     }
-    std::cout << "Collatz's sequence has " << count << " elements" << std::endl;
     return 0;
 }
