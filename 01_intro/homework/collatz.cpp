71812691446f76fa026655ce03cbd19649d6e01d
Dan Reisman <danr@wayve.ai>
2023-03-03 09:16:26 +0000

    Initial solution try for single input

--- 01_intro/homework/collatz.cpp
+++ 01_intro/homework/collatz.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
 int main() {
+    std::string line;
+
+    std::cout << "Enter an integer for n: " << std::flush;
+    std::getline(std::cin, line);
+    int n = std::stoi(line);
+    if (n < 1){
+        std::cerr << "Please enter a positive integer." << std::endl;
+        return 1;
+    }
+    while ( n != 1 )  {
+        std::cout << n << ",";
+        if ( n%2 == 0) {
+            n /= 2;
+        }else{
+            n += 2 * n + 1;
+        }
+    }
     std::cout << "1" << std::endl;
     return 0;
 }
