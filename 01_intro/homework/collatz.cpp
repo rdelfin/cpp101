767eb4d5a88496bce880cefe0069539b8b48eb37
Nico X <ncroce00@gmail.com>
2023-03-04 15:49:21 +0000

    submit homework

--- 01_intro/homework/collatz.cpp
+++ 01_intro/homework/collatz.cpp
@@ -1,6 +1,32 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    std::cout << "1" << std::endl;
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string line;
+
+    // Get first number
+    std::cout << "Give me a number: " << std::flush;
+    std::getline(std::cin, line);
+    unsigned int Collatz_state = std::stoi(line);
+
+    // std::cout << "Collatz Base is " << Collatz_state << std::endl;
+
+    while (Collatz_state != 1)
+    {
+        if (Collatz_state % 2 == 0)
+        {
+            Collatz_state = Collatz_state / 2;
+        }
+        else
+        {
+            Collatz_state = Collatz_state * 3 + 1;
+        }
+        std::cout << Collatz_state << std::endl;
+    }
+
     return 0;
 }
