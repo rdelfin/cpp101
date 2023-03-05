f7839d8f5d62f147f0e3da662346096360d74b82
Nico X <ncroce00@gmail.com>
2023-03-05 12:31:18 +0000

    try to submit homework

--- 01_intro/homework/collatz.cpp
+++ 01_intro/homework/collatz.cpp
@@ -13,6 +13,14 @@ int main()
     std::getline(std::cin, line);
     unsigned int Collatz_state = std::stoi(line);
 
+    if (Collatz_state == 0)
+    {
+        std::cout << "0 is not accepted, give me a better number:" << std::flush;
+        std::getline(std::cin, line);
+        unsigned int Collatz_state = std::stoi(line);
+    } /*this is a bit crap because it only checks once, if the user puts again 0, then it does not catch it,
+    but i do not know how to raise errors in c++ yet, maybe that would have been better? */
+
     // std::cout << "Collatz Base is " << Collatz_state << std::endl;
 
     while (Collatz_state != 1)
