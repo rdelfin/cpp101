181ff6bb7398a0bdf87f15dcb53ff9b251e8739c
arvind <arvind@wayve.ai>
2023-03-05 20:52:50 +0000

    fix tests

--- 02_iterables/homework/wh.cpp
+++ 02_iterables/homework/wh.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 #include <algorithm>
 #include <cstring>
+#include <sstream>
 #include <vector>
 #include "CLI/CLI11.hpp"
 
-bool compare(std::pair<std::string, int> p1, std::pair<std::string, int> p2) {
+bool my_compare_descending(std::pair<std::string, int> p1, std::pair<std::string, int> p2) {
     if (p1.second != p2.second ) 
         return p1.second > p2.second ;
+    return p1.first < p2.first;
+}
+
+bool my_compare_ascending(std::pair<std::string, int> p1, std::pair<std::string, int> p2) {
+    if (p1.second != p2.second ) 
+        return p1.second < p2.second ;
     return p1.first < p2.first;
 }
 
 int main(int argc, char* argv[]) {
     CLI::App app{"Word Counter"};
     std::string line;
+    std::vector<std::string> user_input;
     std::vector<std::string> input_sequence;
     std::vector<std::string> output;
     std::vector<int> repetitions;
@@ -20,24 +28,42 @@ int main(int argc, char* argv[]) {
     int num;
     int elements;
 
-
     app.set_help_all_flag("--help-all", "Expand all help");
     app.add_flag("-r,--reverse", reverse_sequence,
                  "Add this flag in if you want the reverse order");
     app.add_option("-n,--num", num, "Number of elements");
     CLI11_PARSE(app, argc, argv);
 
+    //std::cout << " I got: " << std::endl;
+    //std::cout << reverse_sequence << std::endl;
+    //std::cout << num << std::endl;
+
     while (std::getline(std::cin, line)) {
-        input_sequence.push_back(line) ;
+        user_input.push_back(line) ;
     }
 
-    std::cout << std::endl;
+    if(user_input.empty()){
+        return 0;
+    }
+
+    for(int i=0; i< (int)user_input.size(); i++){
+        std::string word;
+        std::stringstream ss(user_input[i]);
+        while (ss >> word){
+            input_sequence.push_back(word);
+        }
+    }
 
     // Converts everything to lower case and remove end punctuations only
     for(int i=0; i < (int) input_sequence.size(); i++){
         transform(input_sequence[i].begin(), input_sequence[i].end(), input_sequence[i].begin(), ::tolower);
-        if (ispunct(input_sequence[i].back())){
-            input_sequence[i] = input_sequence[i].substr(0, input_sequence[i].length() - 1);
+
+        while(input_sequence[i].length() && ispunct(input_sequence[i].back())){
+            input_sequence[i].pop_back();
+        }
+
+        while(input_sequence[i].length() && ispunct(input_sequence[i].front())){
+            input_sequence[i] = input_sequence[i].substr(1, input_sequence[i].length());
         }
     }
 
@@ -49,7 +75,6 @@ int main(int argc, char* argv[]) {
         else {
             for(int j=0; j < (int)input_sequence.size(); j++){
                 if(j != i){
-                    //std::cout << "i and j are " << i << " " <<  j << std::endl;
                     if(input_sequence[j].compare(input_sequence[i]) == 0){
                        count = count + 1;
                     }
@@ -60,13 +85,17 @@ int main(int argc, char* argv[]) {
         } 
     }
 
-    //std::cout << "Start sorting..." << std::endl;
     std::vector<std::pair<std::string, int>> sorted_pair;
     for(int i=0; i< (int)repetitions.size(); i++){
         sorted_pair.push_back(std::make_pair(output[i], repetitions[i]));
     }
 
-    std::sort(sorted_pair.begin(), sorted_pair.end(), compare);
+    if(reverse_sequence == true){
+        std::sort(sorted_pair.begin(), sorted_pair.end(), my_compare_ascending);
+    }
+    else {
+        std::sort(sorted_pair.begin(), sorted_pair.end(), my_compare_descending);
+    }
 
     if(num == 0){
         elements = (int)repetitions.size();
@@ -75,16 +104,8 @@ int main(int argc, char* argv[]) {
         elements = num;
     }
 
-    int x;
-    if(reverse_sequence == true){
-        x = repetitions.size() - 1;
-    }
-    else{
-        x = 0;
-    }
-
     for(int i=0; i < elements; i++){
-        std::cout << sorted_pair[std::abs(x-i)].first << " " << sorted_pair[std::abs(x-i)].second << std::endl;
+       std::cout << sorted_pair[i].first << " " << sorted_pair[i].second << std::endl;
     }
 
     return 0;
