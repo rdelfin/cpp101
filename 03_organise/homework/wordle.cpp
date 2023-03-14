03ea90fc4aeddf25aaa0c03ba5f84bafa59a252e
arvind <arvind@wayve.ai>
2023-03-14 13:29:24 +0000

    start

--- 03_organise/homework/wordle.cpp
+++ 03_organise/homework/wordle.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "CLI/CLI11.hpp"
 
 // These are all the words you can accept as "valid" when you press enter
 std::vector<std::string> get_valid_wordle_words() {
@@ -16,7 +17,13 @@ std::vector<std::string> get_valid_wordle_words() {
     return words;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+    CLI::App app{"Wordle"};
+    app.set_help_all_flag("--help-all", "Expand all help");
+    app.add_flag("--answer", answer,
+                 "Add this flag in if you want the reverse order");
+    CLI11_PARSE(app, argc, argv);
+
     std::vector<std::string> valid_words = get_valid_wordle_words();
 
     for (size_t i = 0; i < valid_words.size(); i++) {
