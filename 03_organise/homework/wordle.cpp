da45934788725b2655183816aa15291c558e1e57
arvind <arvind@wayve.ai>
2023-03-14 22:08:58 +0000

    no fb

--- 03_organise/homework/wordle.cpp
+++ 03_organise/homework/wordle.cpp
@@ -17,17 +17,75 @@ std::vector<std::string> get_valid_wordle_words() {
     return words;
 }
 
+bool check_valid_answer_word(std::vector<std::string> valid_words, std::string correct_word){
+    bool is_valid = false;
+
+    for(int i=0; i < (int)valid_words.size(); i++){
+        if(correct_word == valid_words[i]){
+            is_valid = true;
+            return is_valid;
+        }
+    }
+    return is_valid;
+}
+
+bool check_legit_user_input(std::string word){
+    bool is_legit_input = false;
+
+    if(word.length() == 5){
+        is_legit_input = true;
+        return is_legit_input;
+    }
+    return is_legit_input;
+}
+
+
+std::string get_user_input(void){
+    std::string user_word;
+    std::cin >> user_word;
+    return user_word;
+}
+
 int main(int argc, char* argv[]) {
+
+    std::string answer;
+    bool feedback;
+    std::string user_input;
+    const int total_attempts = 6;
+
     CLI::App app{"Wordle"};
     app.set_help_all_flag("--help-all", "Expand all help");
-    app.add_flag("--answer", answer,
-                 "Add this flag in if you want the reverse order");
+    app.add_option("--answer, -a", answer,
+                 "Add this flag to feed in the correct word");
+    app.add_flag("-f,--feedback", feedback,
+                 "Add this flag in if you want feedback");
     CLI11_PARSE(app, argc, argv);
 
     std::vector<std::string> valid_words = get_valid_wordle_words();
 
-    for (size_t i = 0; i < valid_words.size(); i++) {
-        std::cout << valid_words[i] << std::endl;
+    if(!check_valid_answer_word(valid_words, answer)){
+        std::cout << "Answer word not valid, please check again." << std::endl;
+        return 3;
     }
-    return 0;
+
+
+    for(int i=0; i < total_attempts; i++){
+        user_input = get_user_input();
+        if(!check_legit_user_input(user_input)){
+            std::cout << "User input not valid, please try again." << std::endl;
+            return 2;
+        }
+
+        if(user_input == answer){
+            std::cout << "CORRECT" << std::endl;
+            return 0;
+        }
+        std::cout << "WRONG" << std::endl;
+    }
+
+    std::cout << "Attempts exceeded" << std::endl;
+    
+
+
+    return 1;
 }
