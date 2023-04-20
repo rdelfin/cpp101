57a846c7143968b9719c5d71d2fbf08216906895
Edgar <edgar@wayve.ai>
2023-04-20 16:15:23 +0000

    Pass all tests

--- 01_intro/homework/collatz.cpp
+++ 01_intro/homework/collatz.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
 int main() {
-    std::cout << "1" << std::endl;
+    // Ask for a number
+    int32_t number;
+    std::cin >> number;
+    std::cout << std::endl;
+
+    // Make sure that the number is positive
+    if ( number <= 0){
+        return -1;
+    }
+
+    // Start printing all the steps
+    std::cout << number << std::endl;
+
+    while (number!=1){
+        if (number%2 == 0){
+            number = number/2;
+            std::cout << number << std::endl;
+        }
+        else {
+            number = (3 * number) + 1;
+            std::cout << number << std::endl;
+        }
+    }
+
     return 0;
 }
