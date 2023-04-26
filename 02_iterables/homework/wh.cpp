f3ff420b89e8108ad7a1d24ce417df3c8cac07ff
Edgar <edgar@wayve.ai>
2023-04-26 14:02:39 +0000

    Pass test_humpty_dumpty_ordered

--- 02_iterables/homework/wh.cpp
+++ 02_iterables/homework/wh.cpp
@@ -5,9 +5,11 @@
 
 int main() {
     std::string line;
+    std::map<std::string, int> map_of_words;
+    std::vector<std::pair<std::string, int>> vector_of_words;
 
     while (std::getline(std::cin, line)) {
-        std::map<std::string, int> map_of_words;
+        
 
         std::stringstream iss(line);
 
@@ -21,19 +23,18 @@ int main() {
                 map_of_words.insert_or_assign(word, count);
             }
         }
-        std::vector<std::pair<std::string, int>> vector_of_words;
-
-        for(std::map<std::string, int>::const_iterator it = map_of_words.begin(); it != map_of_words.end(); ++it) {
-            vector_of_words.push_back(std::make_pair(it->first, it->second));
-        }
-
         
-        std::sort(vector_of_words.begin(), vector_of_words.end(), [](std::pair<std::string, int> pair1, std::pair<std::string, int> pair2)
-        { return ((pair1.second == pair2.second) && (pair1.first <= pair2.first)) || pair1.second > pair2.second; }
-        );
+    }
 
-        for (auto & element : vector_of_words) {
-            std::cout << element.first << " " << element.second << std::endl;
-        }
+    for(std::map<std::string, int>::const_iterator it = map_of_words.begin(); it != map_of_words.end(); ++it) {
+        vector_of_words.push_back(std::make_pair(it->first, it->second));
+    }
+        
+    std::sort(vector_of_words.begin(), vector_of_words.end(), [](std::pair<std::string, int> pair1, std::pair<std::string, int> pair2)
+    { return ((pair1.second == pair2.second) && (pair1.first <= pair2.first)) || pair1.second > pair2.second; }
+    );
+
+    for (auto & element : vector_of_words) {
+        std::cout << element.first << " " << element.second << std::endl;
     }
 }
