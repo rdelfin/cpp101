a851a108198fc0287d59b2ddae9c0b427634f413
Edgar <edgar@wayve.ai>
2023-04-26 15:23:05 +0000

    Pass args

--- 02_iterables/homework/wh.cpp
+++ 02_iterables/homework/wh.cpp
@@ -16,7 +16,28 @@ std::string removePunctuation(std::string str) {
     return str;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+    int n = -1;
+    bool reverse_order = false;
+
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+
+        if (arg == "-n") {
+            if (i + 1 < argc) {
+                n = std::stoi(argv[++i]);
+            } else {
+                std::cerr << "Error: -n requires an argument" << std::endl;
+                return 1;
+            }
+        }
+
+        if (arg == "-r") {
+            reverse_order = true;
+        }
+    }
+
+
     std::string line;
     std::map<std::string, int> map_of_words;
     std::vector<std::pair<std::string, int>> vector_of_words;
@@ -45,12 +66,24 @@ int main() {
     for(std::map<std::string, int>::const_iterator it = map_of_words.begin(); it != map_of_words.end(); ++it) {
         vector_of_words.push_back(std::make_pair(it->first, it->second));
     }
-        
-    std::sort(vector_of_words.begin(), vector_of_words.end(), [](std::pair<std::string, int> pair1, std::pair<std::string, int> pair2)
-    { return ((pair1.second == pair2.second) && (pair1.first <= pair2.first)) || pair1.second > pair2.second; }
-    );
 
+    if (reverse_order) {
+        std::sort(vector_of_words.begin(), vector_of_words.end(), [](std::pair<std::string, int> pair1, std::pair<std::string, int> pair2)
+        { return ((pair1.second == pair2.second) && (pair1.first >= pair2.first)) || pair1.second < pair2.second; }
+        );
+
+    } else {
+        std::sort(vector_of_words.begin(), vector_of_words.end(), [](std::pair<std::string, int> pair1, std::pair<std::string, int> pair2)
+        { return ((pair1.second == pair2.second) && (pair1.first <= pair2.first)) || pair1.second > pair2.second; }
+        );
+    }
+
+    int i = 0;
     for (auto & element : vector_of_words) {
         std::cout << element.first << " " << element.second << std::endl;
+        i++;
+        if (i == n) {
+            break;
+        }
     }
 }
